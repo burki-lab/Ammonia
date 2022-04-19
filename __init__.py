ffba02ad691209ea53f5585b9f085b1b480ee1f1
biothomme <55461592+biothomme@users.noreply.github.com>
2022-04-19 09:26:28 +0200

    Bugs fixed

--- __init__.py
+++ __init__.py
@@ -1,2 +1,1 @@
 import src.utils as dbex_utils
-import src.utils_blast as utils_blast
--- src/__init__.py
+++ src/__init__.py
@@ -1,4 +1,4 @@
 import yaml
-NCBI_CFG_FILE = "../CONFIG_NCBI.yml"
+NCBI_CFG_FILE = "./CONFIG_NCBI.yml"
 
 NCBI_CFGS = yaml.load(open(NCBI_CFG_FILE, "r"), Loader=yaml.BaseLoader)
